355d28f14358c7673e67cd9aaedf05a5a5f7ed4d
Joel <joepotential@gmail.com>
2025-11-15 02:26:23 +0100

    deployment issues fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 import NavBar from "./components/NavBar";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
